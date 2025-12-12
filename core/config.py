544dbac9adac2700a2f7b10368a02b29977aa5b9
Sona <sona.tuuvi@gmail.com>
2025-12-13 00:26:06 +0200

    clean the file

---
+++ core/config.py
@@ -0,0 +1,18 @@
+# config.py
+
+MAX_WIFI_RETRIES = 3
+
+# Wi-Fi
+WIFI_SSID = ""
+WIFI_PASSWORD = ""
+
+
+# MQTT
+MQTT_BROKER = ""
+MQTT_PORT =
+MQTT_CLIENT_ID = "b"
+MQTT_TOPIC_PUB = "kubios/request"
+MQTT_TOPIC_SUB = "kubios/response"       
+MAX_MQTT_RETRIES = 3
+
+MAC_ADDRESS = ""
